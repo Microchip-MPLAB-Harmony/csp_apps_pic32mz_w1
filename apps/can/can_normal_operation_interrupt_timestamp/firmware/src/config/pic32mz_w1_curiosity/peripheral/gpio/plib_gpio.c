d1695436d9a1a619726b016264bbb54350adf190
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-08-20 00:22:01 +0530

    [CAN] Regenerated demo for can_can_normal_operation_interrupt_timestamp_pic32mz_w1_curiosity
    MH3-39476
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/pic32mz_w1_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/pic32mz_w1_curiosity/peripheral/gpio/plib_gpio.c
@@ -83,10 +83,8 @@ void GPIO_Initialize ( void )
     RPB12R = 6;
 
     /* Lock back the system after PPS configuration */
-    SYSKEY = 0x00000000;
-    SYSKEY = 0xAA996655;
-    SYSKEY = 0x556699AA;
     CFGCON0bits.IOLOCK = 1;
+    SYSKEY = 0x00000000;
 
 }
 
