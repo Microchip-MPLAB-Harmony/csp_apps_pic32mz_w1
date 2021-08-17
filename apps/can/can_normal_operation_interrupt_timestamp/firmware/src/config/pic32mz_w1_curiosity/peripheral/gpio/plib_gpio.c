38b4a0954f647a5d420b1dbcff13fff45dde90d0
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-08-18 00:51:34 +0530

    [CAN] Regenerated demo for can_can_normal_operation_interrupt_timestamp_pic32mz_w1_curiosity
    MH3-57659
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/pic32mz_w1_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/can/can_normal_operation_interrupt_timestamp/firmware/src/config/pic32mz_w1_curiosity/peripheral/gpio/plib_gpio.c
@@ -57,23 +57,21 @@
 */
 void GPIO_Initialize ( void )
 {
+
     /* PORTA Initialization */
     ANSELACLR = 0x4000; /* Digital Mode Enable */
-
     /* PORTB Initialization */
     ANSELBCLR = 0x1000; /* Digital Mode Enable */
-
     /* PORTC Initialization */
-
     /* PORTK Initialization */
     LATK = 0x0; /* Initial Latch Value */
     TRISKCLR = 0x2; /* Direction Control */
-
 
     /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
+
     CFGCON0bits.IOLOCK = 0;
 
     /* PPS Input Remapping */
@@ -82,8 +80,9 @@ void GPIO_Initialize ( void )
     /* PPS Output Remapping */
     RPB12R = 6;
 
-    /* Lock back the system after PPS configuration */
+        /* Lock back the system after PPS configuration */
     CFGCON0bits.IOLOCK = 1;
+
     SYSKEY = 0x00000000;
 
 }
