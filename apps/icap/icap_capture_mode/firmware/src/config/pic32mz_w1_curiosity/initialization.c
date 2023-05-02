d259bee84d009eecefdc43e32d99d020848d7a63
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-02 15:46:59 +0530

    [ICAP] Regenerated demo for icap_icap_capture_mode_pic32mz_w1_curiosity
    MH3-78388

--- apps/icap/icap_capture_mode/firmware/src/config/pic32mz_w1_curiosity/initialization.c
+++ apps/icap/icap_capture_mode/firmware/src/config/pic32mz_w1_curiosity/initialization.c
@@ -183,6 +183,8 @@
  ********************************************************************************/
 static void STDIO_BufferModeSet(void)
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2012_R_21_6_DR_3 */
 
     /* Make stdin unbuffered */
     setbuf(stdin, NULL);
@@ -206,9 +208,11 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
     /* Start out with interrupts disabled before configuring any modules */
-    __builtin_disable_interrupts();
+    (void)__builtin_disable_interrupts();
 
     STDIO_BufferModeSet();
 
@@ -239,9 +243,10 @@ void SYS_Initialize ( void* data )
     EVIC_Initialize();
 
 	/* Enable global interrupts */
-    __builtin_enable_interrupts();
+    (void)__builtin_enable_interrupts();
 
 
+    /* MISRAC 2012 deviation block end */
 }
 
 
