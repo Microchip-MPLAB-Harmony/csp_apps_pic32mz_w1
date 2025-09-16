dc80536bc3b2f3d060a0130829820b3197bfb3c3
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 17:00:33 +0000

    [APP] Regenerated demo for spi__slave__spi_write_read__firmware
    MH3C-279

--- apps/spi/slave/spi_write_read/firmware/src/config/pic32mz_w1_curiosity/peripheral/spi/spi_slave/plib_spi2_slave.c
+++ apps/spi/slave/spi_write_read/firmware/src/config/pic32mz_w1_curiosity/peripheral/spi/spi_slave/plib_spi2_slave.c
@@ -54,12 +54,12 @@
 #define SPI2_READ_BUFFER_SIZE            256
 #define SPI2_WRITE_BUFFER_SIZE           256
 
-volatile static uint8_t SPI2_ReadBuffer[SPI2_READ_BUFFER_SIZE];
-volatile static uint8_t SPI2_WriteBuffer[SPI2_WRITE_BUFFER_SIZE];
+static volatile uint8_t SPI2_ReadBuffer[SPI2_READ_BUFFER_SIZE];
+static volatile uint8_t SPI2_WriteBuffer[SPI2_WRITE_BUFFER_SIZE];
 
 
 /* Global object to save SPI Exchange related data */
-volatile static SPI_SLAVE_OBJECT spi2Obj;
+static volatile SPI_SLAVE_OBJECT spi2Obj;
 
 #define SPI2_CON_CKP                        (0UL << _SPI2CON_CKP_POSITION)
 #define SPI2_CON_CKE                        (1UL << _SPI2CON_CKE_POSITION)
