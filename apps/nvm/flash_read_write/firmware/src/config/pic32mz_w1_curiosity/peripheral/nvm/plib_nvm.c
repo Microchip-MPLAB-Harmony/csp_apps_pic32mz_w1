e971890dabb3bef04bcf653868cda1e25cb5e771
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 17:01:55 +0000

    [APP] Regenerated demo for nvm__flash_read_write__firmware
    MH3C-279

--- apps/nvm/flash_read_write/firmware/src/config/pic32mz_w1_curiosity/peripheral/nvm/plib_nvm.c
+++ apps/nvm/flash_read_write/firmware/src/config/pic32mz_w1_curiosity/peripheral/nvm/plib_nvm.c
@@ -90,7 +90,7 @@ typedef struct
     uintptr_t Context;
 }nvmCallbackObjType;
 
-volatile static nvmCallbackObjType nvmCallbackObj;
+static volatile nvmCallbackObjType nvmCallbackObj;
 /* ************************************************************************** */
 /* ************************************************************************** */
 // Section: Local Functions                                                   */
